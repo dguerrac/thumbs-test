63d86372c8dcfc500fc6220492aea86b928c774f
Diego Guerra <dguerrac15@gmail.com>
2020-09-11 00:35:47 -0500

    nav component with responsive design created, underConstruction component and some routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Layout from '../Layout/Layout';
+import UnderConstruction from '../../views/UnderConstruction/UnderConstruction';
 import './App.scss';
 
 const App = () => {
   return (
-    <div className={''}>
-      Rule of Thumb a
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path={'/'} component={''} />
+          <Route exact path={'/past-trials'}>
+            <UnderConstruction title={'Past trials'}/>
+          </Route>
+          <Route exact path={'/how-it-works'}>
+            <UnderConstruction title={'How it works'}/>
+          </Route>
+          <Route exact path={'/login'}>
+            <UnderConstruction title={'Login'}/>
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { useWindowWidth } from '../../utils/hooks'
+import { NavLink } from 'react-router-dom';
+import { AiOutlineSearch, AiOutlineMenu } from 'react-icons/ai';
+import { FiChevronRight } from 'react-icons/fi';
+
+import './Navbar.scss';
+
+const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+  console.log(showNav);
+  const width = useWindowWidth();
+
+  const handleShowNav = () => setShowNav(!showNav);
+  return (
+    <header className={'header'}>
+      <div className={'header__title'}>
+        <h1>
+          <NavLink to={'/'} activeClassName={'selected'} onClick={handleShowNav}>
+            Rule of Thumb
+          </NavLink>  
+        </h1>
+        {width < 768 && (
+          <button className={'header__title_button'} onClick={handleShowNav}>
+            <AiOutlineMenu/>
+          </button>
+        )}
+      </div>
+      <nav className={`header__nav ${showNav && 'show-nav'}`}>
+        { width < 768 && (
+          <button className={'header__nav_button'} onClick={handleShowNav}>
+            <FiChevronRight/>
+          </button>
+        )}
+        <ul>
+          <li>
+            <NavLink to={'/past-trials'} activeClassName={'selected'} onClick={handleShowNav}>
+              Past Trials
+            </NavLink>  
+          </li>
+          <li>
+            <NavLink to={'/how-it-works'} activeClassName={'selected'} onClick={handleShowNav}>
+              How It Works
+            </NavLink>  
+          </li>
+          <li>
+            <NavLink to={'/login'} activeClassName={'selected'} onClick={handleShowNav}>
+              Log In/Sign Up
+            </NavLink>  
+          </li>
+          <li className={'last-child'}>
+            <AiOutlineSearch/>  
+          </li>  
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar;
