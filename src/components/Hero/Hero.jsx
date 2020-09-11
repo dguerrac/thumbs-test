a7a8ea923c48a89d3ca91f43a776dc3bd418aa4e
Diego Guerra <dguerrac15@gmail.com>
2020-09-11 14:54:23 -0500

    initial configurations on Intro component, without responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Intro from '../../components/Intro/Intro';
 import './Hero.scss';
 
 const Hero = () => {
   return (
     <section className={'hero-container'}>
-
+    	<Intro />
     </section>
   )
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
 
-const Home = (props) => {
+const Home = () => {
   return (
     <Hero />
   )
