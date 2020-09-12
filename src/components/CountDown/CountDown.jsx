45cff3fab56113956009f85075b14affc8620d63
Diego Guerra <dguerrac15@gmail.com>
2020-09-11 22:24:47 -0500

    fix responsive among countdown and intro components

--- src/components/CountDown/CountDown.jsx
+++ src/components/CountDown/CountDown.jsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from "react";
 import './CountDown.scss';
 
+const getRemainTime = () => {
+  let now = new Date(),
+    remainTime = (new Date('Sep 31 2020 00:00:00 GMT-0500') - now + 1000) / 1000,
+    remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2), 
+    remainMinutes = ("0" + Math.floor(remainTime / 60 % 60)).slice(-2),
+    remainHours = ("0" + Math.floor(remainTime / 3600 % 24)).slice(-2),
+    remainDays = Math.floor(remainTime / (3600 * 24));
+
+  return {
+    remainTime,
+    remainSeconds,
+    remainMinutes,
+    remainHours,
+    remainDays,
+  }
+}
+
+const totalTimeLeft = getRemainTime();
+
 const CountDown = () => {
 
-  const getRemainTime = () => {
-    let now = new Date(),
-      remainTime = (new Date('Sep 31 2020 00:00:00 GMT-0500') - now + 1000) / 1000,
-      remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2), 
-      remainMinutes = ("0" + Math.floor(remainTime / 60 % 60)).slice(-2),
-      remainHours = ("0" + Math.floor(remainTime / 3600 % 24)).slice(-2),
-      remainDays = Math.floor(remainTime / (3600 * 24));
-
-    return {
-      remainTime,
-      remainSeconds,
-      remainMinutes,
-      remainHours,
-      remainDays,
-    }
-  }
-
   const [timeLeft, setTimeLeft] = useState(getRemainTime());
-  console.log(timeLeft);
 
   useEffect(() => {
     const timerUpDate = setTimeout(() => {
